e67f5482721042b7374eb22bf4814a171eb1aa44
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2024-02-07 21:05:23 -0500

    edit home page header

--- gem-app/src/app/page.js
+++ gem-app/src/app/page.js
@@ -6,10 +6,9 @@ export default function Home() {
   return (
     <div className="row">
       <div className="col-6">
-        <h1 className="header">Supabase Auth + Storage</h1>
+        <h1 className="header">Gemineye of the Tiger</h1>
         <p>
-          Experience our Auth and Storage through a simple profile management example. Create a user
-          profile and upload an avatar image. Fast, simple, secure.
+          Handmade - Jewelry - accessories - objets d'art
         </p>
       </div>
       <div className="col-6 auth-widget">
